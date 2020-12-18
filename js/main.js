a78c2fa74e7511dc2505647c7fd8583bba4792e2
Nigama Vykari <56264990+Nigama-oss@users.noreply.github.com>
2020-12-18 21:46:06 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,91 @@
+
+(() => {
+    const aboutSection = document.querySelector(".about-section"),
+    tabsContainer = document.querySelector(".about-tabs");
+
+    tabsContainer.addEventListener("click", (event) =>{
+        if(event.target.classList.contains("tab-item") &&
+            !event.target.classList.contains("active")){
+        const target = event.target.getAttribute("data-target");
+
+        tabsContainer.querySelector(".active").classList.remove("outer-shadow",
+        "active");
+
+        event.target.classList.add("active", "outer-shadow");
+
+        aboutSection.querySelector(".tab-content.active").classList.remove("active");
+
+        aboutSection.querySelector(target).classList.add("active");
+        } 
+    })
+}) ();
+
+function bodyScrollingToggle(){
+    document.body.classList.toggle("stop-scrolling");
+}
+
+(() => {
+    const filterContainer = document.querySelector(".portfolio-filter"),
+    portfolioItemsContainer = document.querySelector(".portfolio-items"),
+    portfolioItems = document.querySelectorAll(".portfolio-item"),
+    popup = document.querySelector(".portfolio-popup"),
+    prevBtn = popup.querySelector(".pp-prev"),
+    nextBtn = popup.querySelector(".pp-next"),
+    closeBtn = popup.querySelector(".pp-close"),
+    projectDetailsContainer = popup.querySelector(".pp-details"),
+    projectDetailsBtn = popup.querySelector(".pp-project-details-btn");
+    let itemIndex, slideIndex, screenshots;
+
+    filterContainer.addEventListener("click", (event)=>{
+        if(event.target.classList.contains("filter-item") &&
+        !event.target.classList.contains("active")) {
+            
+            filterContainer.querySelector(".active").classList.remove("outer-shadow", "active");
+
+            event.target.classList.add("active", "outer-shadow");
+            const target = event.target.getAttribute("data-target");
+            portfolioItems.forEach((item) => {
+                if(target === item.getAttribute("data-category") || target === 'all'){
+                    item.classList.remove("hide");
+                    item.classList.add("show");
+                }
+                else {
+                    item.classList.remove("show");
+                    item.classList.add("hide");
+                }
+            })
+        }
+    })
+
+    portfolioItemsContainer.addEventListener("click", (event) =>{
+        if(event.target.closest(".portfolio-item-inner")){
+            const portfolioItem = event.target.closest(".portfolio-item-inner").parentElement;
+
+            itemIndex = Array.from(portfolioItem.parentElement.children).indexOf(
+                portfolioItem);
+            
+            screenshots = portfolioItems[itemIndex].querySelector(".portfolio-item-img img").getAttribute("data-screenshots");
+
+            screenshots = screenshots.split(",");
+            slideIndex = 0;
+            popupToggle();
+            popupSlideshow();
+        }
+    })
+
+    closeBtn.addEventListener("click", () =>{
+        popupToggle();
+    })
+
+    function popupToggle() {
+        popup.classList.toggle("open");
+        bodyScrollingToggle();
+    }
+
+    function popupSlideshow(){
+        const imgSrc = screenshots[slideIndex];
+        const popupImg = popup.querySelector(".pp-img");
+        popupImg.src=imgSrc;
+    }
+
+})();
